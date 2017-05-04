54e984a8b9823a46f44a266afbd4541c89c4e270
SE - David Waguespack <se.david@ccpgames.com>
2017-05-04 15:02:13 +0000

    added notes and moved notes to folder

--- app_exam_v1.R
+++ app_exam_v1.R
@@ -17,15 +17,18 @@ draw = corps %>% sample_n(5) %>% unlist(use.names = F) %>% paste(.,collapse=", "
 record <- "select * from edvald_research.umd.corporationApps
 where corporationID in (@{corps})" %>% QueryDB(sql.params = list(corps=draw) )
 
+template = seq(as.Date('2015-01-01'),as.Date('2017-04-01'),by='day') %>% data.frame(d=.)  %>%
+  mutate(m=month(d),y=year(d)) %>% select(-d)
+
 record %>% mutate(applied = as.numeric(status==0),
-                  invited = as.numeric(status==8)) %>% 
+                         invited = as.numeric(status==8)) %>% 
   arrange(corporationID,eventDate) %>% select(-status) %>% 
   mutate(m=month(eventDate),y=year(eventDate)) %>% 
   group_by(corporationID,y,m) %>% 
   summarize(applied=sum(applied),invited=sum(invited),total=n()) %>%
-  right_join(.,,on)
-  
-  ggplot(.,aes(m,total,group=corporationID)) + geom_line()
+  mutate(date = paste(y,m,sep="-")) %>%
+  ggplot(.,aes(date,total)) + geom_line(aes(color=as.factor(corporationID))) +
+  facet_grid(facets = ~corporationID)
 
 
 
---
+++ notes/api_log_notes.md
@@ -0,0 +1,5 @@
+## Meeting Notes
+
+**Date**: May 4, 2017
+**With**: Kjartan and tim
+**RE**: API logs
