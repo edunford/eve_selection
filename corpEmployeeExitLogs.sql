7654fbb8e47e5f02ba0d280761d470d7fa4021e3
SE - David Waguespack <se.david@ccpgames.com>
2017-05-09 17:37:07 +0000

    updated exit_enter SQL script

--- corpEmployeeExitLogs.sql
+++ corpEmployeeExitLogs.sql
@@ -1,18 +1,8 @@
-
-
-select * from hadoop.samples.eventLogs_corporation__UpdateCorporation
-
-
---- need to piece together corporation exit records...
-
--- generate dyadic data for each employee within the rel. period 
--- where the data is organized as: charID,exitCorpID,enterCorpID
--- for each character we then have a record of when they left and entered a corp
--- which I can represent as a flow (migration) from group to group.
-
-select top 100* from ebs_RESEARCH.dbo.crpEmploymentRecordsEx 
-
-select top 100* from edvald_research.umd.crpSampSelection
+-- employee ENTER/EXIT Logs
+	-- generate dyadic data for each employee within the rel. period 
+	-- where the data is organized as: charID,exitCorpID,enterCorpID
+	-- for each character we then have a record of when they left and entered a corp
+	-- which I can represent as a flow (migration) from group to group.
 
 
 SELECT 
@@ -27,65 +17,15 @@ from ebs_RESEARCH.dbo.crpEmploymentRecordsEx
 
 
 
+select top 100* from edvald_research.umd.corpEmployeeExitLogs order by characterID, eventDate
 
 
----- development
-select *,
-	FORMAT(startDate,'yyyy-MM-dd') as enter,
-	format(dateadd(day,-1, startDate),'yyyy-MM-dd') as priorDate
-	into #sss
-	from ebs_RESEARCH.dbo.crpEmploymentRecordsEx 
-	where characterID in (96019134, 95265675, 95482504)
-	order by enter
+-- examine by character
+select top 100* from edvald_research.umd.corpEmployeeExitLogs 
+where characterID in (96019134, 95265675, 95482504)
+order by characterID, eventDate
 
-select * from #ttt order by characterID, eventDate
-select * from #sss order by characterID, startDate
-
-
-
-SELECT 
-startDate as eventDate,
-characterID,
-LAG(corporationID, 1,0) OVER (ORDER BY startDate) AS exitCorp,
-corporationID as enterCorp,
-LAG(corporationName, 1,0) OVER (ORDER BY startDate) AS exitCorpName,
-corporationName as enterCorpName
-from ebs_RESEARCH.dbo.crpEmploymentRecordsEx 
-where characterID = 96019134
-order by startDate
-
-
-with tmp as (
-	select 
-	characterID,
-	corporationID,
-	startDate as granularDate,
-	FORMAT(startDate,'yyyy-MM-dd') as eventDate,
-	format(dateadd(day,-1, startDate),'yyyy-MM-dd') as priorDate
-	from ebs_RESEARCH.dbo.crpEmploymentRecordsEx 
-	where characterID = 96019134
-)
-select 
-t.granularDate,
-t.eventDate,
-t.characterID,
-ch.userID,
-ch.customerID,
-ch.corporationID as exitCorpID,
-t.corporationID as enterCorpID
-into #tmp1
-from ebs_FACTORY.eve.characterHistory ch
-inner join tmp t on (ch.characterID = t.characterID and ch.historyDate = t.priorDate)
-
-drop table #tmp1
-
-select * from #tmp1 order by eventDate
-
-select top 10* from ebs_WAREHOUSE.owner.dimCharacterSCD
-select top 10* from ebs_FACTORY.eve.characterHistory
-
-select min(historyDate) as min,max(historyDate) as max from ebs_FACTORY.eve.characterHistory 
-
-
-
-
+-- examine by corporation (exit rate)
+select top 100* from edvald_research.umd.corpEmployeeExitLogs 
+where exitCorp = 98382684 or enterCorp = 98382684
+order by eventDate
